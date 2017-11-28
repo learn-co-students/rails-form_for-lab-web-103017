73a26edc462302b15918398ee6aca242e0bea3ac
github username <github email address>
2017-11-28 14:49:21 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :school_classes, only: [:new, :create, :index, :show, :edit, :update]
+
+  resources :students, only: [:new, :create, :index, :show, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
