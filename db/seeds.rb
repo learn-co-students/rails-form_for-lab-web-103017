3a9b01366a84b92c48f956551e0063baccfc3609
jjwumaster <jjwumaster@gmail.com>
2017-11-28 17:14:26 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SchoolClass.destroy_all
+Student.destroy_all
+
+mod_1 = SchoolClass.create(title: "Mod 1", room_number: "1")
+mod_2 = SchoolClass.create(title: "Mod 2", room_number: "2")
+
+jon = Student.create(first_name: "Jon", last_name: "Wu")
+kat = Student.create(first_name: "Kat", last_name: "Something")
